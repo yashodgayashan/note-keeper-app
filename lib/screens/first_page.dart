87bd3e10a3cb4c0c9192eb05505031827eaebd84
yashodgayashan <yashodgayashan@gmail.com>
2020-04-12 11:28:42 +0530

    Added first page

---
+++ lib/screens/first_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatefulWidget {
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title:Text( "Note Maker"),
+      ),
+      body: Text("Hi"),
+    );
+  }
+}
