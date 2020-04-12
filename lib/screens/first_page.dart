8d919baab890dafe3d90c999dad64176bb58aae6
yashodgayashan <yashodgayashan@gmail.com>
2020-04-12 11:33:13 +0530

    Added floated action button to the first page

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -6,13 +6,23 @@ class MainMenu extends StatefulWidget {
 }
 
 class _MainMenuState extends State<MainMenu> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title:Text( "Note Maker"),
+        title: Text("Note Maker"),
       ),
-      body: Text("Hi"),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[Text("Add list of items Here")],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("Button clicked");
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
