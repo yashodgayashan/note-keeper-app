266ba923765b2c804855cd587882dd99c37ae4f9
yashodgayashan <yashodgayashan@gmail.com>
2020-04-12 20:49:29 +0530

    updated database helper class

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -81,4 +81,39 @@ class DatabaseHelper {
     }
     return result;
   }
+
+  Future<int> insertNote(Notes note) async{
+    Database db = await this.database;
+    var result = await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  Future<int> updateNote(Notes note) async{
+    var db = await this.database;
+    var result = await db.update(noteTable,note.toMap(),where: "$colId = ?", whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> deleteNote(int id) async{
+    var db = await this.database;
+    int result = await db.rawDelete("DELETE FROM $noteTable WHERE $colId = $id");
+    return result;
+  }
+
+  Future<int> getCount() async{
+    Database db = await this.database;
+    List<Map<String, dynamic>> x = await db.rawQuery("SELECT COUNT(*) FROM $noteTable");
+    int result = Sqflite.firstIntValue(x);
+    return result;
+  }
+
+  Future<List<Notes>> getNoteList({String option = "created", String order="asc"}) async{
+    var noteMapList = await getNoteMapList(option: option , order: order);
+    int count = noteMapList.length;
+    List<Notes> noteList = List<Notes>();
+    for (int i = 0; i< count; i++){
+      noteList.add(Notes.fromMapObject(noteMapList[i]));
+    }
+    return noteList;
+  }
 }
