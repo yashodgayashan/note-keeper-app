48049aceeb65c3ad94316b6df1a244b6dfa14f43
yashodgayashan <yashodgayashan@gmail.com>
2020-04-15 11:21:06 +0530

    Format the note details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:note_maker/screens/first_page.dart';
+import 'package:note_maker/screens/notes_detail.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Note Maker",
-      home: MainMenu(),
+      home: NoteDatails(),
     ));
