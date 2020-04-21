b623a2cbc92255d1ee9ea53f759e015d9e1e707e
yashodgayashan <yashodgayashan@gmail.com>
2020-04-21 08:42:11 +0530

    Added color attriute to category class

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -3,14 +3,19 @@ import 'package:flutter/foundation.dart';
 class Category {
   int _id;
   String _name;
+  String _color;
 
-  Category(this._name);
+  Category(this._name, this._color);
 
   String get name => _name;
 
   int get id => _id;
 
+  String get color => _color;
+
   set name(String newName) => this._name = newName;
+
+  set color(String newColor) => this._color = newColor;
 
   Map<String, dynamic> toMap() {
     var map = Map<String, dynamic>();
@@ -18,11 +23,13 @@ class Category {
       map["id"] = _id;
     }
     map["name"] = _name;
+    map["color"] = _color;
     return map;
   }
 
   Category.fromMapObject(Map<String, dynamic> map) {
     this._id = map["id"];
     this._name = map["name"];
+    this._color = map["color"];
   }
 }
