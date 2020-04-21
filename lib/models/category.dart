fe0859caed1ec8ed03e82eecb2bbb6721fd2e5e8
yashodgayashan <yashodgayashan@gmail.com>
2020-04-21 08:34:57 +0530

    added category class

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/foundation.dart';
+
 class Category {
   int _id;
-  String name;
+  String _name;
 
-  Category({this.name});
+  Category(this._name);
 
+  String get name => _name;
+
+  int get id => _id;
+
+  set name(String newName) => this._name = newName;
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map["id"] = _id;
+    }
+    map["name"] = _name;
+    return map;
+  }
+
+  Category.fromMapObject(Map<String, dynamic> map) {
+    this._id = map["id"];
+    this._name = map["name"];
+  }
 }
